95ec190c463f8282211b0b42517e9f678022a375
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 15:27:55 -0400

    minor bug fix

--- utilities/utilities/urls.py
+++ utilities/utilities/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from django.http import HttpResponse
 
-
-@api_view(('GET',))
-@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
 def empty(request):
-    return Response(data='asdf', status=status.HTTP_200_OK)
+    return HttpResponse('wakemydyno', content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
