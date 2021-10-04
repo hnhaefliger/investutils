d47d5be4bf995cfa64a36cb08bd86ec0009588db
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 15:12:03 -0400

    minor bug fix

--- utilities/utilities/urls.py
+++ utilities/utilities/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.permissions import AllowAny
 
-def empty(request):
-    return Response(status=status.HTTP_200_OK)
+
+class EmptyViewSet(viewsets.ViewSet):
+    permission_classes = (AllowAny,)
+    lookup_url_kwarg = 'ticker'
+
+    def get(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_200_OK)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/listings/', include('listings.urls')),
-    path('wakemydyno.txt', empty)
+    path('wakemydyno.txt', EmptyViewSet)
 ]
