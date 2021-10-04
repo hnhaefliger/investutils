3d8d8f14cff1a8c1bed88d4674d30fd1bc6f9bf9
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 15:21:16 -0400

    add decorators to empty function

--- utilities/utilities/urls.py
+++ utilities/utilities/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 
 
+@api_view(('GET',))
+@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
 def empty(request):
     return Response(data='asdf', status=status.HTTP_200_OK)
 
