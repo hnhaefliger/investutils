d2cbe452f90e54f5b9b678b8da2edceeb302105b
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 15:06:43 -0400

    add wakemydyno.txt

--- utilities/utilities/urls.py
+++ utilities/utilities/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.response import Response
+from rest_framework import status
+
+def empty(request):
+    return Response(status=status.HTTP_200_OK)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/listings/', include('listings.urls')),
+    path('wakemydyno.txt', empty)
 ]
