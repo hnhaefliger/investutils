29fc3cfbab887d608521eadf47544e6992a0b490
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 15:17:37 -0400

    add content to wakemydyno.txt'

--- utilities/utilities/urls.py
+++ utilities/utilities/urls.py
@@ -1,19 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 
 
-class EmptyViewSet(viewsets.ViewSet):
-    permission_classes = (AllowAny,)
-    lookup_url_kwarg = 'ticker'
-
-    def get(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
+def empty(request):
+    return Response(data='asdf', status=status.HTTP_200_OK)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/listings/', include('listings.urls')),
-    path('wakemydyno.txt', EmptyViewSet)
+    path('wakemydyno.txt', empty)
 ]
