b7e7a2fd6b4dafb9166e42a758987ec00e10fdfe
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-14 21:24:57 -0400

    disable cors

--- utilities/utilities/settings.py
+++ utilities/utilities/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
 ]
 
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
+
     'listings',
     'community',
 ]
@@ -58,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'utilities.urls'
