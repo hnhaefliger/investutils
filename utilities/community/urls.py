2c74a5d5bc039e7c5201056e4a038931c420cd31
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-18 10:19:33 -0400

    register stocktwits urls

--- utilities/community/urls.py
+++ utilities/community/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from .views import RedditPostsViewSet, YFinanceTrendingViewSet, StocktwitsWatchlistViewSet, StocktwitsCommentViewSet, GoogleNewsViewSet
+from .views import *
 
 router = routers.DefaultRouter()
 
@@ -7,6 +7,8 @@ router.register('reddit/posts', RedditPostsViewSet, basename='reddit/posts')
 router.register('yfinance/trending', YFinanceTrendingViewSet, basename='yfinance/trending')
 router.register('stocktwits/watchlist', StocktwitsWatchlistViewSet, basename='stocktwits/watchlist')
 router.register('stocktwits/comments', StocktwitsCommentViewSet, basename='stocktwits/comments')
+router.register('stocktwits/sentiment', StocktwitsSentimentViewSet, basename='stocktwits/sentiment')
+router.register('stocktwits/message_volume', StocktwitsMessageVolumeViewSet, basename='stocktwits/message_volume')
 router.register('google/news', GoogleNewsViewSet, basename='google/news')
 
 
