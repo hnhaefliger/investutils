ecddfa59c551c60816acf61409cd8c6f028887a3
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-19 08:11:16 -0400

    debug=False

--- utilities/utilities/settings.py
+++ utilities/utilities/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#False
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
